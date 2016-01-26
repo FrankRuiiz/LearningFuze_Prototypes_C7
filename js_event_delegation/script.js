3ec5956fa91c56e7ff1deab4bb0ddf80ccd4c67d
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-01-26 15:39:30 -0800

    Feature set 2 complete - Frank Ruiz

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,19 +1,27 @@
 // Answer the questions here:
 
 // - What do you think is going to happen?
+//   The new button will also be affected by the click handler
 
 // - What happened?
+//   The inner text for the new button was also consoled out just like the others.
 
 // - Why?
-
+//   The event was delegated to the child, in this case the button element within the #list ul
 //========== Write your code below ===========//
 
-//FEATURE SET 1
-$(document).ready(function(){
 
-    $('#list').on('click','li',function(){
+$(document).ready(function(){
+//FEATURE SET 1
+    var myList = $('#list');
+    myList.on('click','button',function(){
         console.log($(this).text());
+//FEATURE SET 2
     });
+    var newButton = $('<li></li>').html('<button style="margin:10px">Button #5</button>');
+    myList.append(newButton);
+
 });
 
-//FEATURE SET 2
+
+
