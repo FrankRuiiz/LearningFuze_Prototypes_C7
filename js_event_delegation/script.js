6f99009133bb09b69a568dac7febd0621f414c18
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-01-26 16:32:25 -0800

    js event delegation - Frank Ruiz

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,26 +1,44 @@
 // Answer the questions here:
 
 // - What do you think is going to happen?
-//   The new button will also be affected by the click handler
+//   The new button will not be affected by the handler
 
 // - What happened?
-//   The inner text for the new button was also consoled out just like the others.
+//   The click handler logged out all of the clicks, except for the new button
 
 // - Why?
-//   The event was delegated to the child, in this case the button element within the #list ul
+//   The click handler could not read it as it was added dynamically after the page loaded/document ready
 //========== Write your code below ===========//
 
 
 $(document).ready(function(){
+
+
 //FEATURE SET 1
+//    $('#list button').on('click', function(){
+//        console.log($(this).text());
+//    });
+
+////FEATURE SET 2
     var myList = $('#list');
-    myList.on('click','button',function(){
-        console.log($(this).text());
-//FEATURE SET 2
-    });
     var newButton = $('<li></li>').html('<button style="margin:10px">Button #5</button>');
     myList.append(newButton);
 
+////FEATURE SET 3
+    myList.on('click','button',function() {
+        console.log($(this).text());
+    });
+
+
+    //ADDITIONAL CHALLENGE
+    myList.append('<li><a href="http://www.google.com"><button style="margin:10px">Button #6</button></a></li>');
+
+    myList.on('click', 'a', function(){
+        var elem = $(this);
+        if (elem.is("[href^='http']")) {
+            elem.attr('target','_blank');
+        }
+    });
 });
 
 
